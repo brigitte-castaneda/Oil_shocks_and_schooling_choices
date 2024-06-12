68852288be8a94b14ff70824960c26bb784795b4
polanco-jaime <japj182@hotmail.com>
2024-06-12 14:48:54 -0500

    2024-06-12

--- Scripts/R/read_data.R
+++ Scripts/R/read_data.R
@@ -6,10 +6,11 @@ options(scipen=999)
 bq_auth(token = STEP1)
 project_id <- "ph-jabri"
 dataset_id <- "05_Oil_Shocks_and_Schooling_Choices"
-table_id <- "Oil_shock_on_schooling_choices_sbpro_dw"
+table_id <- "Oil_shock_on_schooling_choices"
 
 data = bigrquery::bq_table_download(
   as_bq_table(
   paste0(  project_id, '.' ,  dataset_id   , '.' ,table_id  )
    ),
   n_max  = Inf )
+ 
